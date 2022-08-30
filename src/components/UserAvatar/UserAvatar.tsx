4d087e698614df041d6b541291afbb5554b8b5ad
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-30 11:54:49 +0300

    Update user avatar component

--- src/components/UserAvatar/UserAvatar.tsx
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -1,20 +1,19 @@
 import React, { memo } from 'react';
 import styles from './UserAvatar.module.scss';
 
-// type Props = {
-//   photo: string,
-//   isOnline: boolean,
-// };
+type Props = {
+  photo: string,
+  isOnline: boolean,
+};
 
-export const UserAvatar: React.FC = memo(() => {
-// export const UserAvatar: React.FC<Props> = memo(({ photo, isOnline }) => {
+export const UserAvatar: React.FC<Props> = memo(({ photo, isOnline }) => {
   return (
     <section className={styles.user}>
       here 7
-      {/* <div className={styles.user__photoWrapper}>
+      <div className={styles.user__photoWrapper}>
         <img
           src={photo}
-          alt='User photo'
+          alt='User'
           className={styles.user__photo}
         />
       </div>
@@ -23,7 +22,7 @@ export const UserAvatar: React.FC = memo(() => {
           ? styles.user__statusOnline
           : styles.user__statusOffline
         }
-      /> */}
+      />
     </section>
   );
 });
