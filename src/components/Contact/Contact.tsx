1cee3b104bcbb669ec8182f796b9177560275d6e
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-31 09:37:35 +0300

    Update contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,23 +1,17 @@
 import React, { memo } from 'react';
-import { Contact as ContactType, setSelectedContact } from '../../store/contactsSlice';
-import { useAppDispatch } from '../../store/hooks';
+import { getContacts } from '../../store/contactsSlice';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { dateFormatter } from '../../helpers/dateFormatter';
 import { UserAvatar } from '../UserAvatar';
 import styles from './Contact.module.scss';
+import { setSelectedContact } from "../../store/selectedContact";
 
 type Props = {
-  contact: ContactType,
-  isOnline: boolean,
+  id: number,
 };
 
-export const Contact: React.FC<Props> = memo(({ contact, isOnline }) => {
-  const {
-    id,
-    name,
-    messages,
-    avatar,
-  } = contact;
-
+export const Contact: React.FC<Props> = memo(({ id }) => {
+  const contact = useAppSelector(getContacts).find((contact) => contact.id === id);
   const dispatch = useAppDispatch();
 
   return (
@@ -26,24 +20,28 @@ export const Contact: React.FC<Props> = memo(({ contact, isOnline }) => {
       key={id}
       onClick={() => dispatch(setSelectedContact(id))}
     >
-      <div className={styles.contact__avatar}>
-        <UserAvatar photo={avatar} isOnline={isOnline} />
-      </div>
+      {contact && (
+        <>
+          <div className={styles.contact__avatar}>
+            <UserAvatar photo={contact.avatar} isOnline={contact.isOnline} />
+          </div>
 
-      <h3 className={styles.contact__name}>
-        {name}
-      </h3>
+          <h3 className={styles.contact__name}>
+            {contact?.name}
+          </h3>
 
-      <p className={styles.contact__message}>
-        {messages[messages.length - 1]?.text}
-      </p>
+          <p className={styles.contact__message}>
+            {contact?.messages[contact?.messages.length - 1]?.text}
+          </p>
 
-      <p className={styles.contact__date}>
-        {dateFormatter(
-          messages[messages.length - 1]?.createdAt,
-          'short'
-        )}
-      </p>
+          <p className={styles.contact__date}>
+            {dateFormatter(
+              contact.messages[contact.messages.length - 1]?.createdAt,
+              'short'
+            )}
+          </p>
+        </>
+      )}
     </article>
   );
 });
