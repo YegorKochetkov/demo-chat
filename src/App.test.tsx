28e515b234041a3dc3ce39feee61ec79d3dadf4c
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-31 09:37:09 +0300

    Update chat room component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getAllByText(/here/i);
-  expect(linkElement).toHaveLength(7);
+test('renders contacts', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const linkElement = screen.getByText(/josefina/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- src/components/ChatRoom/ChatRoom.tsx
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -1,60 +1,56 @@
-// import { getContacts, getSelectedContact, setSelectedContact } from '../../store/contactsSlice';
-// import { useAppDispatch, useAppSelector } from '../../store/hooks';
-// import { MessageInput } from '../MessageInput/MessageInput';
-// import { Message } from '../Message/Message';
-// import { UserAvatar } from '../UserAvatar/UserAvatar';
+import { getContacts } from '../../store/contactsSlice';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { UserAvatar } from '../UserAvatar/UserAvatar';
 import { Message } from "../Message";
 import { MessageInput } from "../MessageInput";
 import styles from './ChatRoom.module.scss';
-// import { useMemo } from 'react';
+import { useMemo } from 'react';
+import { getSelectedContact, setSelectedContact } from "../../store/selectedContact";
 
 export const ChatRoom: React.FC = () => {
-  return <p className={styles.chatRoom}>
-    Here
-    <Message />
-    <MessageInput />
-    </p>
-  // const dispatch = useAppDispatch();
-  // const selectedContact = useAppSelector(getSelectedContact);
-  // const contacts = useAppSelector(getContacts);
-  
-  // const contact = useMemo(() => {
-  //   return contacts.find((contact) => 
-  //     selectedContact === contact.id)
-  // }, [selectedContact, contacts]);
+  const dispatch = useAppDispatch();
 
-  // return (
-  //   <section className={
-  //     selectedContact
-  //       ? (styles.chatRoom + ' ' + styles.chatRoom_selectedChat)
-  //       : styles.chatRoom
-  //   }>
-  //     {contact && (
-  //       <header className={styles.chatRoom__header}>
-  //         <UserAvatar photo={contact.avatar} isOnline={contact.isOnline} />
-  //         <p className={styles.chatRoom__userInfo}>
-  //           {contact.name}
-  //         </p>
-  //         <button
-  //           type='button'
-  //           className={styles.chatRoom__backButton}
-  //           onClick={() => dispatch(setSelectedContact(null))}
-  //         />
-  //       </header>
-  //     )}
-  //     <main className={styles.chatRoom__main}>
-  //       {contact?.messages.map((message, index) => 
-  //         <Message
-  //           key={message.userId + index}
-  //           message={message}
-  //         />
-  //       )}
-  //     </main>
-  //     {contact && (
-  //       <footer className={styles.chatRoom__footer}>
-  //         <MessageInput />
-  //       </footer>
-  //     )}
-  //   </section>
-  // );
+  const selectedContactId = useAppSelector(getSelectedContact);
+  const contacts = useAppSelector(getContacts);
+  
+  const contact = useMemo(() => {
+    return contacts.find((contact) => selectedContactId === contact.id)
+  }, [selectedContactId, contacts]);
+
+  return (
+    <>
+    {contact && 
+      <section className={
+        selectedContactId
+          ? styles.chatRoom + ' ' + styles.chatRoom_selectedChat
+          : styles.chatRoom
+      }>
+        <header className={styles.chatRoom__header}>
+          <UserAvatar photo={contact.avatar} isOnline={contact.isOnline} />
+          <p className={styles.chatRoom__userInfo}>
+            {contact.name}
+          </p>
+          <button
+            type='button'
+            className={styles.chatRoom__backButton}
+            onClick={() => dispatch(setSelectedContact(null))}
+          />
+        </header>
+
+        <main className={styles.chatRoom__main}>
+          {contact.messages.map((message, index) => 
+            <Message
+              key={message.userId + index}
+              message={message}
+            />
+          )}
+        </main>
+
+        <footer className={styles.chatRoom__footer}>
+          <MessageInput />
+        </footer>
+      </section>
+    }
+    </>
+  );
 };
