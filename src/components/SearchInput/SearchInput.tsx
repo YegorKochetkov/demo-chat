71fe38a7490e61a6bb688f21917ab17488f031f9
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-31 09:38:50 +0300

    Update search input component

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,30 +1,29 @@
 import React, { memo, useEffect, useState } from 'react';
-// import { useAppDispatch } from '../../store/hooks';
-// import { setSearchQuery } from '../../store/searchQuerySlice';
+import { useAppDispatch } from '../../store/hooks';
+import { setSearchQuery } from '../../store/searchQuerySlice';
 import styles from './SearchInput.module.scss';
 
 export const SearchInput: React.FC = memo(() => {
-  return <p>here 6</p>
-  // const [query, setQuery] = useState('');
-  // const dispatch = useAppDispatch();
+  const [query, setQuery] = useState('');
+  const dispatch = useAppDispatch();
 
-  // useEffect(() => {
-  //   const timeoutId = setTimeout(() => {
-  //     dispatch(setSearchQuery(query));
-  //   }, 500);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      dispatch(setSearchQuery(query));
+    }, 500);
 
-  //   return () => {
-  //     clearTimeout(timeoutId);
-  //   }
-  // }, [query]);
+    return () => {
+      clearTimeout(timeoutId);
+    }
+  }, [dispatch, query]);
 
-  // return (
-  //   <input
-  //     type='search'
-  //     placeholder='Search or start new chat'
-  //     className={styles.searchInput}
-  //     value={query}
-  //     onChange={(event) => setQuery(event.target.value)}
-  //   />
-  // );
+  return (
+    <input
+      type='search'
+      placeholder='Search or start new chat'
+      className={styles.searchInput}
+      value={query}
+      onChange={(event) => setQuery(event.target.value)}
+    />
+  );
 });
